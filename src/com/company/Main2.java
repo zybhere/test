97a268ec4e562fa7ff32930cae01350740217a29
zybhere <zybhere@126.com>
2016-04-25 17:45:01 +0800

    code

---
+++ src/com/company/Main2.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by zybhere on 16-4-25.
+ */
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("这是第二个文件了，测试合并效果");
+    }
+
+
+}
