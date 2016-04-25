21ca7ed5ebd29aaa3094dadaa6545eb9b825f206
zybhere <zybhere@126.com>
2016-04-25 17:45:55 +0800

    code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hahahahah,你好啊");
+        System.out.println("hahahahah,你好啊 啊哈哈哈哈");
     }
 }
